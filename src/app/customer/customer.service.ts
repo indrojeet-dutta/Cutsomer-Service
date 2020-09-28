4304d6388caf216e40f45bfcc623979ed406968a
Indrojeet Dutta <cfsa2269@gmail.com>
2020-09-28 17:25:38 +0530

    changes

--- src/app/customer/customer.service.ts
+++ src/app/customer/customer.service.ts
@@ -3,6 +3,7 @@ import { CustomerListModel } from './customer-list.model';
 export class CustomerService{
   private customerList:CustomerListModel[]=[]
 
+  //fetch customer details
   getCustomerList(){
     if(localStorage.getItem('customers') === null){
       this.customerList = []
@@ -13,6 +14,7 @@ export class CustomerService{
     return this.customerList.sort((a,b) => a.id - b.id)
   }
 
+  //fetch specific customer by ID
   getCustomerByID(id: number){
     if(localStorage.getItem('customers') === null){
       this.customerList = []
@@ -23,12 +25,14 @@ export class CustomerService{
     return this.customerList.find(x=>x.id === id)
   }
 
+  //add new customer
   addCustomer(customer: CustomerListModel){
     this.customerList.push(customer)
     //implementing local storage to add customers
     localStorage.setItem('customers',JSON.stringify(this.customerList))
   }
 
+  //edit customer details
   editCustomer(updateCustomer: CustomerListModel){
     if(localStorage.getItem('customers') === null){
       this.customerList = []
@@ -37,6 +41,7 @@ export class CustomerService{
       this.customerList = JSON.parse(localStorage.getItem('customers'))
     }
 
+   //sorting customer list by ID
    this.customerList.forEach((cur, index) => {
     if(updateCustomer.id === cur.id){
       this.customerList.splice(index, 1)
